8957e6b346cc7ca8486c9d9ae61bf1a47c7b6066
Malloc72P <dase1102@gmail.com>
2018-11-13 17:01:53 +0900

    FinalRelease_MK3 기능개선 로그인시 이메일 대신 프로필이미지를 보여줌.

--- src/util/myUtil.js
+++ src/util/myUtil.js
@@ -1,8 +1,8 @@
 class myUtil{
     constructor(){
         this.serverProtocol =   "http://"
-        this.serverName     =   "localhost"
-        this.serverPortNum  =   ":8090"
+        this.serverName     =   "skynet765.iptime.org"
+        this.serverPortNum  =   ":40346"
         this.projectName    =   "AkashicRecords"
         this.dummyPath      =   "hello"
         this.imgPrefix      =   this.serverProtocol + this.serverName + this.serverPortNum+'/'+this.projectName+'/'
